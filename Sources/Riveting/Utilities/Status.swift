dd0cf2aa67d7327281823c236e3489dbb5fe4b63
Ethan van Heerden <ethan@vpoint.biz>
2025-04-20 13:25:18 -0400

    Rename Statusable to Status

--- Sources/Riveting/Utilities/Status.swift
+++ Sources/Riveting/Utilities/Status.swift
@@ -1,12 +1,12 @@
 //
-//  Statusable.swift
+//  Status.swift
 //  Riveting
 //
 //  Created by Ethan Van Heerden on 3/1/25.
 //
 
-/// Represents a type that can have a loading, loaded, and error state.
-public enum Statusable<Value> {
+/// Represents a type that can have a loading, loaded, and error status states.
+public enum Status<Value> {
     case loading
     case loaded(Value)
     case error(String?)
