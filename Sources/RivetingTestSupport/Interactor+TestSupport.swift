63d0d433156a64f15d3fbab9e4eeeae104fe8ff6
Ethan van Heerden <ethan@vpoint.biz>
2025-04-21 11:16:51 -0400

    Update collect test utility

--- Sources/RivetingTestSupport/Interactor+TestSupport.swift
+++ Sources/RivetingTestSupport/Interactor+TestSupport.swift
@@ -16,33 +16,57 @@ public extension Interacting {
     ///   - count: The number of domains to collect.
     ///   - timeout: The timeout (in seconds) to collect the number of specified domains.
     /// - Returns: An array of the collected domains.
-    func collect(_ count: Int, timeout: TimeInterval = 1) async throws -> [Domain] {
-        var task: Task<Void, Never>?
+    func collect(
+        _ count: Int,
+        performing actions: [Action],
+        timeout: TimeInterval = 1
+    ) async throws -> [Domain] {
         return try await withTimeout(seconds: timeout) {
-            return try await withCheckedThrowingContinuation { continuation in
-                task = Task {
+            return try await withThrowingTaskGroup(of: [Domain].self) { group in
+                // Child task to listen for domain state updates
+                group.addTask {
                     var collectedDomains = [Domain]()
-                    var iterator = self.domainStream.makeAsyncIterator()
+                    var iterator = domainStream.makeAsyncIterator()
                     
                     while collectedDomains.count < count {
-                        guard let domain = await iterator.next() else {
-                            continuation.resume(
-                                throwing: InteractingTestError.unfulfilled(with: collectedDomains, expectedCount: count)
+                        guard let nextDomain = await iterator.next() else {
+                            throw InteractingTestError.unfulfilled(
+                                with: collectedDomains,
+                                expectedCount: count
                             )
-                            return
                         }
-                        
-                        collectedDomains.append(domain)
-                        
-                        if collectedDomains.count == count {
-                            continuation.resume(returning: collectedDomains)
-                            return
-                        }
+                        collectedDomains.append(nextDomain)
+                    }
+                    
+                    return collectedDomains
+                }
+                
+                // Child task to send the actual events
+                group.addTask {
+                    for action in actions {
+                        interact(with: action)
+                    }
+                    return [] // Signify end of the child task
+                }
+                
+                var emittedDomains = [Domain]()
+                for try await domains in group {
+                    if !domains.isEmpty {
+                        group.cancelAll()
+                        emittedDomains = domains
+                        break
                     }
                 }
+                
+                guard emittedDomains.count == count else {
+                    throw InteractingTestError.unfulfilled(
+                        with: emittedDomains,
+                        expectedCount: count
+                    )
+                }
+                
+                return emittedDomains
             }
-        } onTimeout: {
-            task?.cancel()
         }
     }
 }
