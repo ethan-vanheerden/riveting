9201171dc982222c1360dd08fe8b2b0ebca03221
Ethan van Heerden <ethan@vpoint.biz>
2025-04-23 17:50:09 -0400

    Add result builder

--- Sources/RivetingTestSupport/Interacting/Interacting+TestSupport.swift
+++ Sources/RivetingTestSupport/Interacting/Interacting+TestSupport.swift
@@ -8,19 +8,6 @@
 import Riveting
 import Foundation
 
-// MARK: - TestAction
-
-/// Represents an action an interactor can be sent during a testing environement. This is just the iteractor's
-/// defined actions along with a `wait` actions which specifies a waiting period before it can be fired another action.
-public enum TestAction<Interactor> where Interactor: Interacting {
-    case wait(TimeInterval)
-    case action(Interactor.Action)
-    
-    public init (_ action: Interactor.Action) {
-        self = .action(action)
-    }
-}
-
 // MARK: - Interacting+TestSupport
 
 public extension Interacting {
